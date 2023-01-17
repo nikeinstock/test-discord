ff158c339bcd936ccb489e23e70b7d7ae0c67197
nikeinstock <122600956+nikeinstock@users.noreply.github.com>
2023-01-16 19:34:55 -0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,22 +1,18 @@
-import { Client, Events, GatewayIntentBits } from 'discord.js'
+import { Client } from 'discord.js'
 import * as dotenv from 'dotenv'
 
 dotenv.config()
 
-const client = new Client({
-  intents: [
-    GatewayIntentBits.Guilds,
-    GatewayIntentBits.GuildMessages,
-    GatewayIntentBits.MessageContent,
-  ],
+const client = new Client()
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`)
 })
 
-client.on(Events.ClientReady, () =>
-  console.log(`Logged in as ${client.user.tag}!`)
-)
-
-client.on(Events.MessageCreate, async (message) => {
-  console.log(message.content)
+client.on('message', (msg) => {
+  if (msg.content === '!ping') {
+    msg.reply('pong!')
+  }
 })
 
 client.login(process.env.TOKEN)
